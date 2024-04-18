71f90ebba53fad262ccc72e4dc4ac881c724ee58
RohithAndanala <andanala.rohith@gmail.com>
2024-04-18 01:55:36 -0400

    Added Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import "../App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import prsn from "../assets/prsn.png";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <section className="banner">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7} className="bannerText">
+            <h1>LeadMe.</h1>
+            <h3>A friendly way to find a job</h3>
+          </Col>
+          <Col xs={12} md={6} xl={5} className="prsnImage">
+            <div className="prsn">
+              <img src={prsn} />
+            </div>
+          </Col>
+        </Row>
+        <Button className="btn rounded" variant="outline-primary">
+          Learn More
+        </Button>
+      </Container>
+    </section>
+  );
 }
 
 export default Banner;
