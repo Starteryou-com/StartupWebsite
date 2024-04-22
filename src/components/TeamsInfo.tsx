1085ba69fda4c8b233fad45394168e3485149833
RohithAndanala <andanala.rohith@gmail.com>
2024-04-22 00:03:05 -0400

    modified team component

---
+++ src/components/TeamsInfo.tsx
@@ -0,0 +1,46 @@
+import sampleimg from "../assets/team/rohith.jpg";
+
+export const members = [
+  {
+    name: "Venkata Sai Rohith",
+    path: sampleimg,
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, s",
+    insta: "https://www.instagram.com/",
+  },
+  {
+    name: "Anjali singh",
+    path: sampleimg,
+    about:
+      "Lorem ipsum dolor sit amet, consectetur aminim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+  {
+    name: "Bharadwaj",
+    path: sampleimg,
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+  {
+    name: "Venkata Sai Rohith",
+    path: sampleimg,
+    about:
+      "Lorem ipsum dolor sit ameUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+  {
+    name: "Venkata Sai Rohith",
+    path: sampleimg,
+    about:
+      "Lorem ipsum dolor sna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+  {
+    name: "Venkata Sai Rohith",
+    path: sampleimg,
+    about:
+      "Lorem ipsum dolor sitliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+  {
+    name: "Venkata Sai Rohith",
+    path: sampleimg,
+    about:
+      "Lorem ipsum doloriqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  },
+];
