39e02e4786db9cda0fa97a6cbf12a28e0818f54c
221910305006 <70358842+221910305006@users.noreply.github.com>
2024-05-28 18:30:07 -0400

    Updated Nav Bar

--- src/components/Navibar.tsx
+++ src/components/Navibar.tsx
@@ -1,23 +1,21 @@
-import { Container, NavLink, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, NavLink, Navbar, Nav } from "react-bootstrap";
 
 function Navibar() {
   return (
     <div>
-      <Navbar expand="lg" className=" shadow-5-strong navibar" fixed="top">
+      <Navbar expand="lg" className=" shadow-5-strong navibar" >
         <Container>
-          <Navbar.Brand href="#home">Starteryou</Navbar.Brand>
+          <Navbar.Brand href="#home" className="nav-starteryou">Starteryou</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <NavLink href="#home">Home</NavLink>
-              <NavLink href="#features">Features</NavLink>
-              <NavDropdown title="Know More" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#aboutUs">About Us</NavDropdown.Item>
-                <NavDropdown.Item href="#mission">Our Mission</NavDropdown.Item>
-                <NavDropdown.Item href="#vision">Our Vision</NavDropdown.Item>
-                <NavDropdown.Item href="#team">Our Team</NavDropdown.Item>
-              </NavDropdown>
-              <NavLink href="#contact">Contact us</NavLink>
+              <NavLink href="#home" className="nav-everything">Home</NavLink>
+              <NavLink href="#features" className="nav-everything">Features</NavLink>
+              <NavLink href="#aboutUs" className="nav-everything">About Us</NavLink>
+              <NavLink href="#mission" className="nav-everything">Our Mission</NavLink>
+              <NavLink href="#vision" className="nav-everything">Our Vision</NavLink>
+              <NavLink href="#team" className="nav-everything">Our Team</NavLink>
+              <NavLink href="#contact" className="nav-everything">Contact us</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
