27f8483377f7281e26439267dd0b29360edeaafd
Nikshep A Kulli <48430792+nikshepkulli@users.noreply.github.com>
2024-06-10 17:29:34 -0400

    our features added.

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,16 +1,17 @@
 import Navibar from "../components/Navibar";
-import { Container } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import "../App.css";
 import Banner from "../components/Banner";
 
 import Featuredvideos from "../components/Featuredvideos";
 import Footer from "../components/Footer";
+import Ourfeatures from "../components/Ourfeatures";
 function Homepage() {
   return (
     <Container id="Homepage">
       <Navibar />
       <Banner />
-
+      <Ourfeatures />
       <Featuredvideos />
       <Footer />
     </Container>
