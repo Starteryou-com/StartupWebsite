923eebb6b543f5787ed5f31403a080086bc686dc
connectleadme <167219530+connectleadme@users.noreply.github.com>
2024-06-10 18:20:04 -0400

    Update Jobcard.tsx

--- src/components/Jobcard.tsx
+++ src/components/Jobcard.tsx
@@ -140,11 +140,10 @@ const Jobcard: React.FC<JobcardProps> = ({
   applyLink,
 }) => {
   // Your component code here
-const Jobcard = ({ image, title, description, applyLink }) => {
   return (
     <Card>
       <Image image={image} />
-      <div style={{ flex: 1 }}>
+      <div style={{flex: 1}}>
         <TitleText title={title} />
         <DescriptionText description={description} />
         <ApplyButton applyLink={applyLink} />
@@ -154,3 +153,4 @@ const Jobcard = ({ image, title, description, applyLink }) => {
 };
 
 export default Jobcard;
+
